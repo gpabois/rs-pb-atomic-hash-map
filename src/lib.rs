229ec5cb49b72c980af6f46d05cf66c25b489485
PABOIS Gaël <gael.pabois@gmail.com>
2024-12-17 11:15:48 +0100

    first version 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{alloc::{alloc, dealloc, Layout}, hash::{Hash, Hasher as _}, ptr::NonNull, sync::{atomic::{AtomicUsize, Ordering}, Arc}};
+use std::{alloc::{alloc, dealloc, Layout}, borrow::Borrow, hash::{Hash, Hasher as _}, marker::PhantomData, mem::MaybeUninit, ops::Deref, ptr::NonNull, sync::{atomic::{AtomicUsize, Ordering}, Arc}};
 
 use pb_atomic_linked_list::{prelude::AtomicLinkedList as _, AtomicLinkedList};
 
@@ -50,20 +50,20 @@ impl<K: Hash, V> Bucket<K, V> {
 
 /// A hash table
 struct Table<K: Hash,V> {
-    buckets: NonNull<Bucket<K,V>>,
+    buckets: NonNull<[Bucket<K,V>]>,
     buckets_layout: Layout,
     capacity: usize,
     length: AtomicUsize
 }
 
+unsafe impl<K: Hash,V> Sync for Table<K,V> {}
+unsafe impl<K: Hash,V> Send for Table<K,V> {}
+
 impl<K: Hash, V> Drop for Table<K,V> {
     fn drop(&mut self) {
         unsafe {
-            for i in 0..self.capacity {
-                self
-                    .buckets
-                    .add(i)
-                    .drop_in_place();
+            for bucket in self.buckets.as_mut().iter_mut() {
+                std::ptr::from_mut(bucket).drop_in_place();
             }
 
             dealloc(self.buckets.cast().as_ptr(), self.buckets_layout);
@@ -76,17 +76,21 @@ impl<K: Hash,V> Table<K,V> {
         let buckets_layout = Layout::array::<Bucket<K,V>>(capacity).unwrap();
 
         unsafe {
-            let buckets: NonNull<Bucket<K, V>> = NonNull::new(
-                alloc(buckets_layout)
-                .cast::<Bucket<K,V>>()
-            ).unwrap();
+            let raw_buckets_base_ptr = alloc(buckets_layout).cast::<MaybeUninit<Bucket<K,V>>>();
+            let raw_buckets_slice_ptr= std::ptr::slice_from_raw_parts_mut(
+                raw_buckets_base_ptr,
+                capacity
+            );
 
-            // Initialise all buckets.
-            for i in 0..capacity {
-                let bucket_ptr = buckets.add(i);
-                *bucket_ptr.as_ptr() = Bucket::new();
+            if let Some(mut_buckets_slice_ptr) = raw_buckets_slice_ptr.as_mut() {
+                // Initialise all buckets.
+                for bucket in mut_buckets_slice_ptr {
+                    bucket.write(Bucket::new());
+                }
             }
 
+            let buckets = NonNull::new(std::mem::transmute(raw_buckets_slice_ptr)).unwrap();
+   
             let length = AtomicUsize::new(0);
 
             Self {
@@ -98,6 +102,7 @@ impl<K: Hash,V> Table<K,V> {
         }
         
     }
+    
 
     pub fn len(&self) -> usize {
         return self.length.load(Ordering::Relaxed)
@@ -105,10 +110,8 @@ impl<K: Hash,V> Table<K,V> {
 
     /// Insert a new value in the bucket.
     pub fn insert(&self, key: K, value: V) {
-        let hashed_key = hash(&key);
-        let bucket_key = hashed_key % self.capacity;
         unsafe {
-            let bucket = self.buckets.add(bucket_key).as_mut();
+            let bucket = self.get_bucket_ptr(&key).as_mut();
             if bucket.insert(key, value) {
                 self.length.fetch_add(1, Ordering::Relaxed);
             }
@@ -116,18 +119,102 @@ impl<K: Hash,V> Table<K,V> {
     }
 
     /// Retrieve the bucket which might contain the value behind the key.
-    unsafe fn get_bucket(&self, key: &K) -> NonNull<Bucket<K,V>> {
+    unsafe fn get_bucket_ptr(&self, key: &K) -> NonNull<Bucket<K,V>> {
         let mut hasher = std::collections::hash_map::DefaultHasher::new();
         key.hash(&mut hasher);
         let hashed_key = hasher.finish() as usize;
         let bucket_key = hashed_key % self.capacity;
-        return self.buckets.add(bucket_key)
+        let bucket_ptr = std::ptr::from_mut(
+            self
+                .buckets
+                .as_ptr()
+                .as_mut()
+                .unwrap()
+                .get_mut(bucket_key)
+                .unwrap()
+        );
+
+        return NonNull::new(bucket_ptr).unwrap()
     }
 
 
     unsafe fn get_raw_entry_ptr(&self, key: &K) -> Option<NonNull<Entry<K,V>>> {
-        self.get_bucket(key).as_ref().get_raw_entry_ptr(key)
+        self
+            .get_bucket_ptr(key)
+            .as_ref()
+            .get_raw_entry_ptr(key)
     }
+}
+
+pub struct ValueIter<'a, K: Hash + 'a, V: 'a>(EntryIter<'a, K, V>);
+
+impl<'a, K: Hash + 'a, V: 'a> Iterator for ValueIter<'a, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|entry| &entry.value)
+    }
+}
+
+
+pub struct Iter<'a, K: Hash + 'a, V: 'a>(EntryIter<'a, K, V>);
+
+impl<'a, K: Hash + 'a, V: 'a> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|entry| (&entry.key, &entry.value))
+    }
+}
+
+struct EntryIter<'a, K: Hash + 'a, V: 'a> {
+    _phantom: PhantomData<&'a ()>,
+    buckets: NonNull<[Bucket<K, V>]>,
+    current_bucket_iter: Option<pb_atomic_linked_list::Iter<'a, Entry<K, V>>>,
+    current_bucket_key: usize
+}
+
+impl<'a, K: Hash + 'a, V: 'a> Iterator for EntryIter<'a, K, V> {
+    type Item = &'a Entry<K, V>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            if let Some(iter) = &mut self.current_bucket_iter {
+                if let Some(entry) = iter.next() {
+                    return Some(entry)
+                }
+                else {
+                    self.current_bucket_key += 1;
+                    if self.current_bucket_key >= self.buckets.len() {
+                        return None
+                    }                    
+                    let bucket = self.buckets.as_ref().get(self.current_bucket_key).unwrap();
+                    self.current_bucket_iter = Some(bucket.0.iter());
+                    self.next()
+                }
+
+            } else {
+                return None
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ValueRef<'a, T>(&'a T);
+
+impl<'a, T: Clone> ValueRef<'a, T> {
+    pub fn to_owned(value: Self) -> T {
+        value.0.clone()
+    }
+}
+
+impl<'a, T> Deref for ValueRef<'a, T> {
+    type Target = T;
+    
+    fn deref(&self) -> &Self::Target {
+        self.0
+    }    
 }
 
 pub struct AtomicHashMap<K: Hash, V>(Arc<Table<K, V>>);
@@ -141,7 +228,32 @@ impl<K: Hash, V> Clone for AtomicHashMap<K, V> {
 impl<K: Hash, V> AtomicHashMap<K, V> {
     /// Creates a new atomic hash map
     pub fn new(capacity: usize) -> Self {
-        Self(Arc::new(Table::new(capacity)))
+        Self(
+            Arc::new(
+                Table::new(capacity)
+            )
+        )
+    }
+
+    fn iter_entries(&self) -> EntryIter<'_, K, V> {
+        unsafe {
+            EntryIter {
+                _phantom: PhantomData,
+                buckets: self.0.buckets,
+                current_bucket_iter: self.0.buckets.as_ref().get(0).map(|bucket| bucket.0.iter()),
+                current_bucket_key: 0  
+            }
+        }
+    }
+
+    /// Iterate over all values in the hash map
+    pub fn iter_values(&self) -> ValueIter<'_, K, V> {
+        ValueIter(self.iter_entries())
+    }
+
+    /// Iterate over all key/value pair in the hash map 
+    pub fn iter(&self) -> Iter<'_, K, V> {
+        Iter(self.iter_entries())
     }
 
     /// Returns the length 
@@ -161,9 +273,12 @@ impl<K: Hash, V> AtomicHashMap<K, V> {
     /// Borrow the value behind the key
     /// 
     /// Always returns the last version in the linked list.
-    pub fn borrow<'a>(&'a self, key: &K) -> Option<&'a V> {
+    pub fn borrow<'a, Q: Borrow<K>>(&'a self, key: Q) -> Option<ValueRef<'a, V>> {
         unsafe {
-            self.get_raw_value_ptr(key).map(|value: NonNull<V>| value.as_ref())
+            self
+            .get_raw_value_ptr(key.borrow())
+            .map(|value| value.as_ref())
+            .map(ValueRef)
         }
     }
 
@@ -172,18 +287,79 @@ impl<K: Hash, V> AtomicHashMap<K, V> {
     /// # Unsafe
     /// Because of obvious reasons
     pub unsafe fn get_raw_value_ptr(&self, key: &K) -> Option<NonNull<V>> {
-        self.0.get_raw_entry_ptr(key)
-        .map(|mut entry| NonNull::new_unchecked(std::ptr::from_mut(&mut entry.as_mut().value)))
+        self.0
+        .get_raw_entry_ptr(key)
+        .map(|mut entry| 
+            NonNull::new(
+                std::ptr::from_mut(
+                    &mut entry.as_mut().value
+                )
+            ).unwrap()
+        )
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::AtomicHashMap;
+    use std::{collections::HashSet, thread};
+
+    use crate::{AtomicHashMap, ValueRef};
 
     #[test]
     fn test_borrow_unexisting_value() {
-        let map = AtomicHashMap::<u32, u32>::new(10);
-        assert_eq!(map.borrow(&10).copied(), None);
+        let mut map = AtomicHashMap::<u32, u32>::new(100);
+        map.insert(20, 30); 
+        assert_eq!(map.borrow(&10), None);
+    }
+
+    #[test]
+    fn test_borrow_existing_value() {
+        let mut map = AtomicHashMap::<u32, u32>::new(100);
+        map.insert(10, 20); 
+        assert_eq!(map.borrow(&10).map(ValueRef::to_owned), Some(20));
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut map = AtomicHashMap::<u32, u32>::new(100);
+        let mut expected_entries = HashSet::<(u32, u32)>::default();
+        for i in 0..1_000 {
+            map.insert(i, i);
+            expected_entries.insert((i,i));
+        }
+
+        let got = map.iter().map(|(k, v)| (*k, *v)).collect::<HashSet::<_>>();
+        assert_eq!(got, expected_entries)
+    }
+
+    #[test]
+    fn test_multiple_threads() {
+        let map = AtomicHashMap::<u32, u32>::new(100);
+        let mut map1 = map.clone();
+        let mut map2 = map.clone();
+
+        let mut expected_entries = HashSet::<(u32, u32)>::default();
+        for i in 0..=20_000 {
+            expected_entries.insert((i, i));
+        }
+
+
+        let j1 = thread::spawn(move || {
+            for i in 0..=10_000 {
+                map1.insert(i, i);
+            }
+        });
+
+        let j2 = thread::spawn(move || {
+            for i in 10_001..=20_000 {
+                map2.insert(i, i);
+            }
+        });
+
+        j1.join().unwrap();
+        j2.join().unwrap();
+
+        let got = map.iter().map(|(k, v)| (*k, *v)).collect::<HashSet::<_>>();
+        assert_eq!(got, expected_entries)
     }
 }
